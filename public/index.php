fb49de98db8e794ba598bb4c8d7db0fa356a143b
NikolaiProgramist <nikluk2005@mail.ru>
2025-02-28 21:41:51 +0300

    Add named routes

--- public/index.php
+++ public/index.php
@@ -11,10 +11,12 @@ $container->set('renderer', fn () => new \Slim\Views\PhpRenderer(__DIR__ . '/../
 $app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
+$router = $app->getRouteCollector()->getRouteParser();
+
 $app->get('/', function ($request, $response) {
     $response->getBody()->write('Welcome to Slim!');
     return $response;
-});
+})->setName('/');
 
 $app->get('/users', function ($request, $response) {
     $users = json_decode(file_get_contents('cache/users'), true);
@@ -24,30 +26,30 @@ $app->get('/users', function ($request, $response) {
     $params = ['users' => $resultUsers];
 
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
-});
+})->setName('users');
 
 $app->get('/users/new', function ($request, $response) {
     return $this->get('renderer')->render($response, 'users/new.phtml');
-});
+})->setName('createUser');
 
-$app->post('/users', function ($request, $response) {
+$app->post('/users', function ($request, $response) use ($router) {
     $users = json_decode(file_get_contents('cache/users'), true);
     $data = $request->getParsedBodyParam('user');
     $id = random_int(1, 100);
     $users[] = ['id' => $id, 'nickname' => $data['nickname'], 'email' => $data['email']];
     file_put_contents('cache/users', json_encode($users));
 
-    return $response->withRedirect('/users', 302);
+    return $response->withRedirect($router->urlFor('users'), 302);
 });
 
 $app->get('/courses/{id}', function ($request, $response, array $args) {
     $id = $args['id'];
     return $response->write("Course id: {$id}");
-});
+})->setName('course');
 
 $app->get('/users/{id}', function ($request, $response, array $args) {
     $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
     return $this->get('renderer')->render($response, 'users/show.phtml', $params);
-});
+})->setName('user');
 
 $app->run();
