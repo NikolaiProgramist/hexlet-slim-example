dde3ee75a74264fd97c0f19a657739b8bcbbfb84
NikolaiProgramist <nikluk2005@mail.ru>
2025-03-06 11:34:13 +0300

    The necessary ones have been added new getters and setters to the class Car.

--- src/Car.php
+++ src/Car.php
@@ -4,13 +4,51 @@ namespace Nikolai\HexletSlimExample;
 
 class Car
 {
-    public function fromArray()
-    {
+    private int $id;
+    private string $make;
+    private string $model;
 
+    public static function fromArray(array $carProperties): Car
+    {
+        $car = new Car();
+        $car->setMake($carProperties['make']);
+        $car->setModel($carProperties['model']);
+
+        return $car;
+    }
+
+    public function exists(): bool
+    {
+        return $this->id;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMake(): string
+    {
+        return $this->make;
+    }
+
+    public function getModel(): string
+    {
+        return $this->model;
     }
 
     public function setId(int $id): void
     {
+        $this->id = $id;
+    }
 
+    public function setMake(string $make): void
+    {
+        $this->make = $make;
+    }
+
+    public function setModel(string $model): void
+    {
+        $this->model = $model;
     }
 }
